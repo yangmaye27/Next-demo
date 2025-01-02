d3f4025d52247c3412e0cf419b8c936240bdc7b7
yangmaye27 <yangmaye27@163.com>
2025-01-02 19:16:20 +0800

    修改主页

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
           >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Course~~Maye
             </a>
             , brought to you by Vercel.
           </p>
